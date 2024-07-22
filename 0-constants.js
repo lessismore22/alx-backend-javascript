9dd0ef9654ad2f436d5dc0dcbc3ceb4669172657
lessismore22 <charlesigbe0@gmail.com>
2024-07-22 17:09:24 +0100

    0-constants.js

---
+++ 0-constants.js
@@ -0,0 +1,22 @@
+taskFirst() {
+	var task = 'I prefer const when I can.';
+	return task;
+}
+let task() {
+return 'is okay';
+	}
+}export function taskFirst() {
+    var task = 'I prefer const when I can.';
+    return task;
+  }
+  
+  export function getLast() {
+    return ' is okay';
+  }
+  
+  export function taskNext() {
+    var combination = 'But sometimes let';
+    combination += getLast();
+  
+    return combination;
+  }
